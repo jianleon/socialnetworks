51e85fa60d66fc40cfba2879699079f7f6aa46e5
Jimmy <jianleon@utp.edu.co>
2016-10-18 10:44:55 -0500

    Plantilla base de proyecto

---
+++ SocialNetworks/app/src/main/java/com/kogi/socialnetworks/InstagramFragment.java
@@ -0,0 +1,26 @@
+package com.kogi.socialnetworks;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InstagramFragment extends Fragment {
+
+
+    public InstagramFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.instagram_fragment, container, false);
+    }
+
+}
---
+++ SocialNetworks/app/src/main/java/com/kogi/socialnetworks/TwitterFragment.java
@@ -0,0 +1,26 @@
+package com.kogi.socialnetworks;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TwitterFragment extends Fragment {
+
+
+    public TwitterFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.twitter_fragment, container, false);
+    }
+
+}
